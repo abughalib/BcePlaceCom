2dd70a6efe453097027011b80ebd49141f930555
Abu Ghalib <abughalib64@gmail.com>
2021-06-03 02:15:11 +0530

    splash screen without delay

--- app/src/main/java/com/dev/bcepedia/bceplacecom/SplashActivity.kt
+++ app/src/main/java/com/dev/bcepedia/bceplacecom/SplashActivity.kt
@@ -19,56 +19,12 @@ import com.dev.bcepedia.bceplacecom.ui.onBoarding.OnBoardingActivity
 
 class SplashActivity : AppCompatActivity() {
 
-  private lateinit var mDelayHandler: Handler
-
-  private val mRunnable: Runnable = Runnable {
-
-    val intent = Intent(applicationContext, OnBoardingActivity::class.java)
-    startActivity(intent)
-    finish()
-  }
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
-    setContentView(R.layout.activity_splash)
-    supportActionBar?.hide()
+    startActivity(Intent(this, MainActivity::class.java))
+    finish()
 
-    //Initializing the Handler
-    mDelayHandler = Handler(Looper.getMainLooper())
-
-    //Navigate with delay
-    mDelayHandler.postDelayed(mRunnable, SPLASH_DELAY)
-
-  }
-
-  private fun hideSystemUI() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-      window.setDecorFitsSystemWindows(false)
-      window.insetsController?.let {
-        it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-        it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-      }
-    } else {
-      @Suppress("DEPRECATION")
-      window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
-              or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-              or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-              or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-              or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-              or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
-    }
-  }
-
-  override fun onWindowFocusChanged(hasFocus: Boolean) {
-    super.onWindowFocusChanged(hasFocus)
-    if (hasFocus) {
-      hideSystemUI()
-    }
-  }
-
-  companion object{
-    const val SPLASH_DELAY: Long = 2000
   }
 
 }
